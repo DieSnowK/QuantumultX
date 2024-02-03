ce8b005aadc5455dfb156b2a7e16063b65cd3d96
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-02-03 17:21:06 +0800

    wufan

---
+++ wufan.js
@@ -0,0 +1,35 @@
+var body = $response.body; // 声明一个变量body并以响应消息体赋值
+var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
+
+obj.svip_level = 1;
+obj.vip_level = 1;
+obj.is_sign = true;
+
+// {
+//   "error" : 0,
+//   "data" : {
+//     "user_info" : {
+//       "avatar_src" : "http://ctimg2018.5fun.net/upload/images/headportrait/public/system_15.png",
+//       "uid" : 139963209,
+//       "svip_level" : 1,
+//       "nickname" : "papa108677690",
+//       "officialAccount" : false,
+//       "papa_money" : 500,
+//       "vip_level" : 1,
+//       "unread_message" : 0,
+//       "is_sign" : false
+//     },
+//     "posts_list" : [
+
+//     ]
+//   }
+// }
+
+body = JSON.stringify(obj); // 重新打包回json字符串
+$done(body); // 结束修改
+
+
+
+
+
+
