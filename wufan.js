261f8774595a49ca00aae584524ab73132969d10
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-02-13 14:17:07 +0800

    wufan

--- wufan.js
+++ wufan.js
@@ -1,28 +1,42 @@
 var body = $response.body; // 声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
 
-obj.svip_level = 2;
-obj.vip_level = 2;
-obj.is_sign = true;
+obj.is_vip = "9A0684792021D73BE42B71491469ADFA";
 
 // {
-//   "error" : 0,
-//   "data" : {
-//     "user_info" : {
-//       "avatar_src" : "http://ctimg2018.5fun.net/upload/images/headportrait/public/system_15.png",
-//       "uid" : 139963209,
-//       "svip_level" : 1,
-//       "nickname" : "papa108677690",
-//       "officialAccount" : false,
-//       "papa_money" : 500,
-//       "vip_level" : 1,
-//       "unread_message" : 0,
-//       "is_sign" : false
-//     },
-//     "posts_list" : [
+//     "error" : 0,
+//         "data" : {
+//         "user_info" : {
+//             "reg_time" : "1611934605",
+//                 "expired_time" : 0,
+//                     "member_state" : 0,
+//                         "is_vip" : "8F8CBF4D4F96CF9B18E232903B5409FE"
+//         },
+//         "error_code" : "",
+//             "data_info" : [
 
-//     ]
-//   }
+//             ],
+//                 "is_success" : true,
+//                     "error_msg" : ""
+//     }
+// }
+
+// {
+//     "error" : 0,
+//         "data" : {
+//         "user_info" : {
+//             "reg_time" : "1630032617",
+//                 "expired_time" : 0,
+//                     "member_state" : 2,
+//                         "is_vip" : "9A0684792021D73BE42B71491469ADFA"
+//         },
+//         "error_code" : "",
+//             "data_info" : [
+
+//             ],
+//                 "is_success" : true,
+//                     "error_msg" : ""
+//     }
 // }
 
 body = JSON.stringify(obj); // 重新打包回json字符串
